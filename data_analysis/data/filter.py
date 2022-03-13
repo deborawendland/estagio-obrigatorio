e820fecd7f7f3bac4bc8d99926ba333a12a9d795
Debora Daniela Wendland Amorim <deborawendland@gmail.com>
2022-03-13 17:04:16 +0100

    chores: add data base filters

---
+++ data_analysis/data/filter.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+def drop_column(base, attributes):
+    if len(attributes['drop_cols']) > 0:
+        print('Dropping {cols} colums'.format(cols=attributes['drop_cols']))
+        return base.drop(columns=attributes['drop_cols'])
+    elif len(attributes['select_cols']) > 0:
+        print('Keeping {cols} columns'.format(cols=[attributes['select_cols']]))
+        return base.loc[:,[attributes['select_cols']]]
+        
+
+def map_columns(base, attributes):
+    for map_col in attributes['map_cols']:
+        name = map_col['name']
+        print('Formating column {col}.'. format(col=name))
+        base[name] = base[name].map(map_col['value'])
+    return base
+
+
+def nan_values(base):
+    print('Replacing nan values.')
+    base.replace(np.nan, 0, inplace = True)
+    return base
+
+
+def target_attribute(base, attribute):
+    print('Selecting target attribute {attr}.'.format(attr=attribute))
+    y = base[attribute]
+    x = base.drop([attribute], axis=1)
+    return x, y
